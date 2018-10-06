b909bdd0d2b07deda5009bcc7454ff1ed0429af5
luolongfei <l-luo@onetapbuy.co.jp>
2018-10-06 15:01:28 +0800

    首次提交

---
+++ resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,5 @@
+<hr>
+
+<div class="footer">
+    Powered by <a href="https://github.com/DirectoryLister/DirectoryLister">Directory Lister</a>. Deployed by <a href="https://github.com/luolongfei">mybsdc</a>.</a>
+</div>
